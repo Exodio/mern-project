ed6ee9a98a5ec9a3393622cce7f0b7d08fbfb2ba
exodio <minchev.adrian13@gmail.com>
2021-03-09 20:46:11 +0200

    Validate new user data
    -Install and setup body-parser
    -Install and setup cookie-parser
    - Create post register schema
    - Save new user model
    - Test post new user via Postman
    - Validate, if userData received in MongoDB Atlas Clusters collection

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
 
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+const { User } = require('./models/user');
+
 const port = 5000;
 const connectionString = 'mongodb+srv://exodio:13241324@cluster0.rspdu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-
-const mongoose = require('mongoose');
 
 mongoose.connect(connectionString, {
         useNewUrlParser: true
@@ -12,9 +16,19 @@ mongoose.connect(connectionString, {
     .then(() => console.log('DB Connected Successful!'))
     .catch((err) => console.log(err));
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
 
-app.get('/', (req, res) => {
-    res.send("Home page");
+app.post('/api/users/register', (req, res) => {
+    const user = new User(req.body);
+
+    user.save((err, userData) => {
+        if (err) return res.json( {success: false, err });
+        
+        return res.status(200).json({ success: true });
+    });
+
 });
 
 app.listen(port);
--- models/user.js
+++ models/user.js
@@ -30,7 +30,7 @@ const userSchema = mongoose.Schema({
   },
 });
 
-const User = mongoose.Model("User", userSchema);
+const User = mongoose.model("User", userSchema);
 
 module.exports = {
   User,
