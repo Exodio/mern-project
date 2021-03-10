5bdb39be8968acfc791173b9776e7ee54b6e7bb4
exodio <minchev.adrian13@gmail.com>
2021-03-10 18:59:42 +0200

    Setup authentication and middleware
    -Implement login
    -Compare user passwords
    -Bcrypt the recived password if it matches
    -Generate the token via jsonWebToken method
    -Save the new generated token to the cookie section
    -Implement authentication middleware

--- models/user.js
+++ models/user.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
+const jwt = require('jsonwebtoken');
+
 
 const userSchema = mongoose.Schema({
   name: {
@@ -38,10 +40,11 @@ userSchema.pre('save', function (next) {
   if (user.isModified('password')) {
     bcrypt.genSalt(saltRounds, function (err, salt) {
       if (err) return next(err);
-      
+
       bcrypt.hash(user.password, salt, function (err, hash) {
         if (err) return next(err);
         user.password = hash;
+        next();
       });
     });
   } else {
@@ -49,8 +52,35 @@ userSchema.pre('save', function (next) {
   }
 });
 
+userSchema.methods.comparePassword = function(plainPassword, cb) {
+  bcrypt.compare(plainPassword, this.password, function(err, isMatch) {
+    if(err) return cb(err);
+    cb(null, isMatch);
+  });
+}
+
+userSchema.methods.generateToken = function(cb) {
+  var user = this;
+  var token = jwt.sign(user._id.toHexString(), 'secret');
+
+  user.token = token;
+  user.save(function (err, user){
+    if(err) return cb(err);
+    cb(null, user);
+  });
+}
+
+userSchema.statics.findByToken = function (token, cb) {
+  var user = this;
+
+  jwt.verify(token, 'secret', function(err, decode) {
+    user.findOne({ "_id": decode, "token": token }, function (err, user){
+      if (err) return cb(err);
+      cb(null, user);
+    });
+  });
+} 
+
 const User = mongoose.model("User", userSchema);
 
-module.exports = {
-  User,
-};
+module.exports = { User };
