586e17ef0dd66b6420ea115cbd7a69903e3329a4
exodio <minchev.adrian13@gmail.com>
2021-03-14 16:56:07 +0200

    Configurate and setup Redux
    - Setup and configurate actions and reducers's state

--- client/src/index.js
+++ client/src/index.js
@@ -5,11 +5,26 @@ import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import promiseMiddleware from 'redux-promise';
+import reduxThunk from 'redux-thunk';
+
+import Reducer from './components/reducers';
+const basicReduxStore = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, reduxThunk)(createStore);
 ReactDOM.render(
-    <BrowserRouter>
-    <App />,
-    </BrowserRouter>,
-  document.getElementById('root')
+  <Provider store={createStoreWithMiddleware(Reducer, basicReduxStore)}>
+  <BrowserRouter>
+
+    <App />
+
+    </BrowserRouter>
+
+    </Provider>
+    
+  , document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
