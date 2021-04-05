a1401ce73b36d0dabff62090b97e762ffc2b4853
exodio <minchev.adrian13@gmail.com>
2021-04-05 16:13:58 +0300

    Display private keys
    -display private keys in order for SoftUni
    quaestors to have access to AtlasDB

---
+++ client/src/config/token.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mapboxAPI: "pk.eyJ1IjoiZXhvZGlvIiwiYSI6ImNrbXlvM3YxajA1ejQydm8zcXN2Y3NkaWIifQ.ZqkbLgj1LFPL52kUDxjQ_Q",
+};
---
+++ server/config/dev.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: "mongodb+srv://exodio:13241324@cluster0.rspdu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+};
